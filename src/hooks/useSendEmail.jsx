11f3e324d186b54632702020b9f49e84f78553b0
Whiz7z <gjevstratov@gmail.com>
2024-07-28 19:45:07 +0100

    third  commit

--- src/hooks/useSendEmail.jsx
+++ src/hooks/useSendEmail.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 
-
+// ?? "template_qvrwkg8"
 
 const useSendEmail = () => {
   const [submitting, setSubmitting] = useState(false);
@@ -14,7 +14,7 @@ const useSendEmail = () => {
     await emailjs
       .send(
         import.meta.env.VITE_REACT_APP_SERVICE_ID ,
-        import.meta.env.VITE_REACT_APP_TEMPLATE_ID 
+        import.meta.env.VITE_REACT_APP_TEMPLATE_ID,
         {
           phone: phoneNumber,
           email: email,
