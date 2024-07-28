17662352d914f16a8dde587f23ad9ab3cfa74605
Whiz7z <gjevstratov@gmail.com>
2024-07-28 19:36:55 +0100

    second  commit

--- src/hooks/useSendEmail.jsx
+++ src/hooks/useSendEmail.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 
+
+
 const useSendEmail = () => {
   const [submitting, setSubmitting] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(false);
 
   const sendEmail = async (phoneNumber, email, name) => {
    
@@ -12,21 +13,21 @@ const useSendEmail = () => {
     setSubmitting(true);
     await emailjs
       .send(
-        import.meta.env.VITE_REACT_APP_SERVICE_ID ?? "template_qvrwkg8",
-        import.meta.env.VITE_REACT_APP_TEMPLATE_ID ?? "service_z7ymplh",
+        import.meta.env.VITE_REACT_APP_SERVICE_ID ,
+        import.meta.env.VITE_REACT_APP_TEMPLATE_ID 
         {
           phone: phoneNumber,
           email: email,
           name: name,
         },
-        import.meta.env.VITE_REACT_APP_USER_PUBLIC_KEY ?? "8T-L_e_i2B3yhhe-K"
+        import.meta.env.VITE_REACT_APP_USER_PUBLIC_KEY 
       )
       .then(() => {
         setSubmitting(false);
         setSuccess(true);
       });
   };
-  return [sendEmail, submitting, success, isEmpty];
+  return [sendEmail, submitting, success];
 };
 
 export default useSendEmail;
